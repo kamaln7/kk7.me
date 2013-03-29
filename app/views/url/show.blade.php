acd3271321f6a1d86d9e3f25b1dc89b100b379f2
KamalN7 <kamal@kamalnasser.net>
2013-03-30 00:47:59 +0300

    Missing files that belong to the last commit

---
+++ app/views/url/show.blade.php
@@ -0,0 +1,15 @@
+@extends('template.master')
+
+@section('content')
+    <h2>{{ Html::link(URL::action('UrlController@redirect', [$url->hash])) }}</h2>
+    <h5>Usage</h5>
+    <p>
+        HTML: <code>{{ e(Html::link(URL::action('UrlController@redirect', [$url->hash]))) }}</code><br>
+        BBCode: <code>[url]{{ URL::action('UrlController@redirect', [$url->hash]) }}[/url]</code>
+    </p>
+    <h5>Statistics</h5>
+    <p>
+        Created {{ $created }}.<br>
+        {{ $url->views }} views.
+    </p>
+@stop
